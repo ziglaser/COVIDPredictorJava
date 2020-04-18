9a0bebb8c92e07b706a15637bc1e602ab675c2fe
PotatoTax <conor@johngregg.org>
2020-04-18 17:15:22 -0500

    Spring formatting

---
+++ src/main/java/com/covidpredictor/covidpredictor/CovidpredictorApplication.java
@@ -0,0 +1,13 @@
+package com.covidpredictor.covidpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidpredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidpredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/covidpredictor/covidpredictor/CovidpredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidpredictor.covidpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidpredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
